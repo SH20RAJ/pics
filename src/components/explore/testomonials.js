41d91a3064795000fc15c4dadf02f08420b0ace5
sh20raj <shaswatraj3@gmail.com>
2024-07-04 19:03:20 +0530

    chore: Update testimonial author name

--- src/components/explore/testomonials.js
+++ src/components/explore/testomonials.js
@@ -28,7 +28,7 @@ export default function Testomonials() {
             />
             <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
               <div className="pr-3 font-medium text-gray-900 dark:text-white">
-                Deepshree Gupta
+                Sh Raj
               </div>
               <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">
                 CEO at Shade
