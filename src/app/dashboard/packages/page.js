5fa7a3ab85518c7a2a6375b61b22a08a82dc1b96
sh20raj <shaswatraj3@gmail.com>
2024-07-04 20:09:58 +0530

    chore: Update react dependency to version 18.3.1
    This commit updates the react dependency in the package.json and package-lock.json files to version 18.3.1. This update ensures compatibility with the latest version of React and includes any bug fixes or improvements introduced in the new version.

--- src/app/dashboard/packages/page.js
+++ src/app/dashboard/packages/page.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import { Packages } from '@/components/packages'
 
 export default function page() {
   return (
     <div>
-      
+      <Packages />
     </div>
   )
 }
