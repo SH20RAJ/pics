0acfb3f540a6116d124e8a258dc7c9b73734bb17
sh20raj <shaswatraj3@gmail.com>
2024-07-04 15:44:13 +0530

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.21.9
      project name = pics
      framework = next
      framework cli = create-next-app@14.1.0
      package manager = npm@10.8.1
      wrangler = wrangler@3.62.0
      git = 2.45.1

--- src/app/(auth)/join/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {SignIn} from "@/components/auth/signIn"
-export default function page() {
-  return (
-    <div>
-      <SignIn/>
-    </div>
-  )
-}
--- src/components/NavBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NavBar() {
-  return (
-    <div>
-      NavBar
-    </div>
-  )
-}
