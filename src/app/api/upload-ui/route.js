7c478ee444b84beb6b380a191bfc3bcff92a8c4f
sh20raj <shaswatraj3@gmail.com>
2024-07-06 12:39:04 +0530

    API Key Upload
    This commit refactors the upload route in the `upload-ui/route.js` file to use the `auth` function from the `@/auth` module for user authentication. It also updates the code to use the `prisma` module for database operations related to image uploading.
    
    These changes improve the security and reliability of the upload functionality by leveraging established authentication and database handling mechanisms.

--- src/app/api/upload-ui/route.js
+++ src/app/api/upload-ui/route.js
@@ -1,67 +1,17 @@
+import { auth } from "@/auth";
 import { convertToCloudinaryUrl, uploadFileToDiscord } from "@/lib/funcs";
 import prisma from "@/prisma";
 import { NextResponse } from "next/server";
 
 // POST handler for the upload route
 export const POST = async (req) => {
+  const session = await auth();
   try {
     // Parse the request body
     const formData = await req.formData();
     const file = formData.get("file");
     let path = formData.get("path") || "./";
     let tags = formData.get("tags") ? formData.get("tags").split(",") : [];
-
-    // get the barear token api key and from the api key get the user id
-    const token = req.headers.get("Authorization");
-    const apiKey = token.split(" ")[1];
-    const api = await prisma.apiKey.findUnique({
-      where: {
-        key: apiKey,
-      },
-      select: {
-        user: {
-          select: {
-            id: true,
-          },
-        },
-      },
-    });
-
-    // if the api key is not found return an error
-    if (!api) {
-      return NextResponse.json({ error: "API Key not found" }, { status: 401 });
-    }
-    
-    const userId = api.user.id;
-
-    // check if the user quota is exceeded or not
-    const user = await prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-      select: {
-        quota: true,
-        // select images count only
-        _count: {
-          select: {
-            images: true,
-          },
-        },
-      },
-    });
-
-    // if the user quota is exceeded return an error
-    if (user.quota < user._count.images) {
-      return NextResponse.json({ error: "User quota exceeded" }, { status: 401 });
-    }
-
-
-    // if the user is not found return an error
-    if (!user) {
-      return NextResponse.json({ error: "User not found" }, { status: 401 });
-    }
-
-    //if user has 
 
     // Upload the file to Discord
     const filename = file.name;
@@ -79,7 +29,7 @@ export const POST = async (req) => {
         uniqueId: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
         user: {
           connect: {
-            id: userId,
+            id: session.user.userId,
           },
         },
       },
--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,17 +1,74 @@
-import { auth } from "@/auth";
 import { convertToCloudinaryUrl, uploadFileToDiscord } from "@/lib/funcs";
 import prisma from "@/prisma";
 import { NextResponse } from "next/server";
 
 // POST handler for the upload route
 export const POST = async (req) => {
-  const session = await auth();
+  const url = new URL(req.url)
+
   try {
     // Parse the request body
     const formData = await req.formData();
     const file = formData.get("file");
     let path = formData.get("path") || "./";
     let tags = formData.get("tags") ? formData.get("tags").split(",") : [];
+
+    const host = req.headers.get('host');
+    const protocol = req.headers.get('x-forwarded-proto') || 'http';
+    const currentUrl = `${protocol}://${host}`;
+
+
+
+    // get the barear token api key and from the api key get the user id
+    const token = req.headers.get("Authorization");
+    const apiKey = token.split(" ")[1];
+    const api = await prisma.apiKey.findUnique({
+      where: {
+        key: apiKey,
+      },
+      select: {
+        user: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    });
+
+    // if the api key is not found return an error
+    if (!api) {
+      return NextResponse.json({ error: "API Key not found" }, { status: 401 });
+    }
+
+    const userId = api.user.id;
+
+    // check if the user quota is exceeded or not
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        quota: true,
+        // select images count only
+        _count: {
+          select: {
+            images: true,
+          },
+        },
+      },
+    });
+
+    // if the user quota is exceeded return an error
+    if (user.quota < user._count.images) {
+      return NextResponse.json({ error: "User quota exceeded" }, { status: 401 });
+    }
+
+
+    // if the user is not found return an error
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 401 });
+    }
+
 
     // Upload the file to Discord
     const filename = file.name;
@@ -20,7 +77,7 @@ export const POST = async (req) => {
     console.log(imageUrl, path, tags);
     tags = tags.join(",");
     // Create response data
-    const data = await prisma.image.create({
+    let data = await prisma.image.create({
       data: {
         url: imageUrl,
         path: path,
@@ -29,11 +86,12 @@ export const POST = async (req) => {
         uniqueId: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
         user: {
           connect: {
-            id: session.user.userId,
+            id: userId,
           },
         },
       },
     });
+    data.url = `${url.origin}/api/images/${data.uniqueId}`;
 
     // Return the response
     return NextResponse.json(data);
