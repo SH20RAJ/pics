adaaaca5c05f46ae86af3bc9a85b86ad4e49e9e5
sh20raj <shaswatraj3@gmail.com>
2024-07-08 16:22:02 +0530

    Custom 404 and Contact Page

---
+++ src/app/contact/page.js
@@ -0,0 +1,10 @@
+import { redirect } from 'next/navigation'
+
+export default function page() {
+    redirect('https://docs.pics.shade.cool/contact')
+  return (
+    <div className=''>
+      Contact to sh20raj@gmail.com
+    </div>
+  )
+}
